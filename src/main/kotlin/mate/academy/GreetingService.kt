19179f85cd3bc5771270cc479a2a51c5ec8b4dde
Andriy Beha <apgroup.ua@gmail.com>
2024-05-17 11:27:16 +0200

    Implemented

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -3,5 +3,8 @@ package mate.academy
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
         // TODO: implement it
+        return clientNames.mapNotNull { name ->
+            name?.let { "Hello, $it!" }
+        }
     }
 }
