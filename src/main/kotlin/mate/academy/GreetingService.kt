20a1f18abe4b7561f2683ab4df1d8b20401b6864
Nonzeq <bgdn.kobylchak@gmail.com>
2024-06-03 13:55:41 +0300

    added

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -1,7 +1,16 @@
 package mate.academy
 
 class GreetingService {
+    companion object {
+        const val MESSAGE = "Hello, %s!"
+    }
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        val result : MutableList<String> = mutableListOf()
+        clientNames.forEach {
+            it?.let {
+                result.add(MESSAGE.format(it))
+            }
+        }
+        return result
     }
 }
