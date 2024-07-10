9c9f9ba17d20e44104c8cb7b88334d798e75de02
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-07-10 10:54:58 +0300

    added hw solution

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -2,6 +2,12 @@ package mate.academy
 
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        val personalGreetings = mutableListOf<String>()
+        clientNames.forEach {
+            it?.let {
+                personalGreetings.add("Hello, $it!")
+            }
+        }
+        return personalGreetings
     }
 }
