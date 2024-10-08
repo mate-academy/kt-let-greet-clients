266be0fd357ba626094dd72521608a3fe488387b
Zhdanov Anton <anton1992xxxx@gmail.com>
2024-10-08 16:18:58 +0300

    "implement GreetingService class"

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -1,7 +1,17 @@
 package mate.academy
 
+private const val GREETING_TEMPLATE = "Hello, %s!"
+
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        val greetings = mutableListOf<String>()
+
+        clientNames.forEach { name ->
+            name?.let {
+                greetings.add(GREETING_TEMPLATE.format(it))
+            }
+        }
+
+        return greetings
     }
 }
