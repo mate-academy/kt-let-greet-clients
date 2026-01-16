3ab104b5f06c4143bc722482088e1b1a8c376d15
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2026-01-16 12:50:12 -0400

    let

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -2,6 +2,8 @@ package mate.academy
 
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        return clientNames.mapNotNull { name ->
+            name?.let { "Hello, $it!" }
+        }
     }
 }
