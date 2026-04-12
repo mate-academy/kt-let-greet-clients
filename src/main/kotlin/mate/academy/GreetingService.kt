4d4a955845b85b454d51e11995fb59ae843687fe
mc704ed <mykyta.churylov@student.tuke.sk>
2026-04-12 19:03:06 +0200

    update

--- src/main/kotlin/mate/academy/GreetingService.kt
+++ src/main/kotlin/mate/academy/GreetingService.kt
@@ -2,6 +2,14 @@ package mate.academy
 
 class GreetingService {
     fun getGreetings(clientNames: List<String?>): List<String> {
-        // TODO: implement it
+        val list: MutableList<String> = mutableListOf()
+        clientNames.let {
+            for (i in clientNames) {
+                if (i != null) {
+                    list.add("Hello, $i!")
+                }
+            }
+        }
+        return list
     }
 }
